3a7596711b4446b18bdea617787aadee6bfd5a21
Chandan-Gupta12 <82501616+Chandan-Gupta12@users.noreply.github.com>
2021-05-24 22:16:18 +0530

    Add files via upload

--- path.cpp
+++ path.cpp
@@ -1,65 +1,40 @@
 #include<bits/stdc++.h>
+#include <string>
 using namespace std;
 
 #define ll long long int 
-int path=0;
-void mazepath(int i,int j,int n,int m,string osf){
-    if(i==n-1 and j==m-1){
-        path++;
-        cout<<osf<<endl;
-        return;
-    }
-    if(i>=n or j>=m){
-        return;
-    }
-    mazepath(i,j+1,n,m,osf+"R");
-    mazepath(i+1,j,n,m,osf+"D");
-    mazepath(i+1,j+1,n,m,osf+"//");//Diagonal
-}
 
-#include<bits/stdc++.h>
-using namespace std;
 
-#define ll long long int 
-int path=0;
-void mazepath(int i,int j,int n,int m,string osf){
-    if(i==n-1 and j==m-1){
-        path++;
-        cout<<osf<<endl;
-        return;
-    }
-    if(i>=n or j>=m){
-        return;
-    }
-    mazepath(i,j+1,n,m,osf+"R");
-    mazepath(i+1,j,n,m,osf+"D");
-    mazepath(i+1,j+1,n,m,osf+"//");//Diagonal
-}
-void dicepath(int n,int i,string osf){
-    if(i==n-1){
-        cout<<osf<<endl;
-        return;
-    }
-    if(i>n-1){
-        return;
-    }
-    for (int j =1;j<=6; ++j)
-    {
-        dicepath(n,i+j,osf+to_string(j)+"-->");
-    }
-}
-
+        
 int main(){
     ios::sync_with_stdio(0);   
     cin.tie(0);   
+
+
 
     #ifndef ONLINE_JUDGE
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     #endif
     
-    mazepath(0,0,3,3,"");
-    cout<<"Total path="<<path<<"\n";
-    dicepath(7,0,"");
-    return 0;
+    int n;cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=1;i<n;i++){
+        int key=a[i];
+        int j=i-1;
+        while(j>=0 and a[j]>key){
+            a[j+1]=a[j];
+            j=j-1;
+        }
+        a[j+1]=key;
+    }
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+ return 0; 
+    
 }
+   
