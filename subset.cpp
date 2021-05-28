ea496fd59d710319e4a81de2fa9bc8d1e17459ce
Chandan-Gupta12 <82501616+Chandan-Gupta12@users.noreply.github.com>
2021-05-28 21:31:07 +0530

    Add files via upload

---
+++ subset.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+#include <string>
+using namespace std;
+
+#define ll long long int 
+
+
+// bool isitsafe(vector<vector<bool>> grid,int row,int col,int n){
+//     for(int i=row-1;i>=0;i--){   //column check
+//         if(grid[i][col]) return false;
+//     }
+//     for(int i=row-1,j=col-1;i>=0 and j>=0;i--,j--){ //left upper diagonal
+//         if(grid[i][j]) return false;
+//     }
+//     for(int i=row-1,j=col+1;i>=0 and j<n;i--,j++){ //right upper diagonal
+//         if(grid[i][j]) return false;
+//     }
+//     return true;
+// }
+
+void display(vector<vector<int>> &grid,int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<grid[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+bool isitsafe(vector<vector<int>> &grid,int i,int j,int n,vector<vector<bool>> &visited){
+    return i>=0 and j>=0 and i<n and j<n and visited[i][j]==false;
+}
+
+void KnightTour(vector<vector<int>> &grid, int i,int j,int n,int count,vector<vector<bool> > &visited){
+    //Base case
+    if(count==n*n-1){
+        grid[i][j]=count;
+        display(grid,n);
+        cout<<"\n\n";
+        return;
+    }
+    if(count>=n*n){
+        return;
+    }
+    int xdir[8]={-2,-2,-1,-1,2,2,1,1};
+    int ydir[8]={1,-1,2,-2,1,-1,2,-2};
+    visited[i][j]=true;
+    grid[i][j]=count;
+    for(int k=0;k<8;k++){
+        if(isitsafe(grid,i+xdir[k],j+ydir[k],n,visited)){
+            KnightTour(grid,i+xdir[k],j+ydir[k],n,count+1,visited);
+        }
+    }
+    grid[i][j]=-1;
+    visited[i][j]=false;
+}
+
+int main(){
+    ios::sync_with_stdio(0);   
+    cin.tie(0);   
+
+
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    int n;cin>>n;
+    vector<vector<int> > grid(n,vector<int> (n,-1));
+    vector<vector<bool> > visited(n,vector<bool> (n,false));
+
+    KnightTour(grid,0,0,n,0,visited);
+    
+    return 0;
+}
+   
