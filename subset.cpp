daaa9ce69ace647980a0abd37e2dd12030062452
Chandan-Gupta12 <82501616+Chandan-Gupta12@users.noreply.github.com>
2021-06-23 19:14:23 +0530

    Add files via upload

---
+++ subset.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define pb push_back
+#define loop(i,a,b) for(auto i=a;i<=b;++i)
+#define ll long long int
+#define endl "\n"
+#define mod 1000000007
+#define all(v)  v.begin(),v.end()
+
+
+int phi(int n){
+    int res=n;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            res=res/i;
+            res=res*(i-1);
+            while(n%i==0){
+                n=n/i;
+            }
+        }
+    }
+    if(n>1){
+      res=res/n;
+      res*=(n-1);
+    }
+    return res;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+   
+//Algorithm for Euler totient Function.
+
+    int n;
+    cin>>n;
+    cout<<phi(n)<<endl;
+
+
+ 
+return 0;
+}
